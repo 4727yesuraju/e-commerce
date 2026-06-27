795154bcbe6e9009782ef9324dae35045f25661a
yesu <4727yesuraju@gmail.com>
2026-06-27 16:32:27 +0530

    feat: update dependencies and integrate Sentry for error tracking
    - Added new dependencies: @sentry/react, @tanstack/react-query, @tailwindcss/vite, lucide-react, and react-router.
    - Removed App.css and migrated styles to Tailwind CSS.
    - Implemented a loading state with PageLoader component.
    - Created Layout component to structure the application with Navbar and Footer.
    - Added SentryErrorFallback for error handling and SentryUserSync to sync user context with Sentry.
    - Updated main.jsx to include Sentry initialization and error boundary.
    - Removed unused assets (hero.png, react.svg, vite.svg).

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import "./App.css";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import {
+  Show,
+  SignInButton,
+  SignUpButton,
+  useAuth,
+  UserButton,
+} from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import Layout from "./components/Layout";
 
 function App() {
+  const { isLoaded } = useAuth();
+  if (!isLoaded) return <PageLoader />;
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
@@ -13,7 +22,10 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
+
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button>
+    </Layout>
   );
 }
 
